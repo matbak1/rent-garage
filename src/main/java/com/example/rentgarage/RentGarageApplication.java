5d17935eef890da746bf8b872fde5fece8e8ba80
matbak1 <mateusz.bakon@gmail.com>
2020-11-15 20:46:26 +0100

    initial project

---
+++ src/main/java/com/example/rentgarage/RentGarageApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentgarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentGarageApplication.class, args);
+	}
+
+}
